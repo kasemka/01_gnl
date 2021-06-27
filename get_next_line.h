0530e823323897a7c93bd5b7ad7f495e43a8a249
Lorent Elle <lelle@va-j3.kzn.21-school.ru>
2021-06-27 12:24:51 +0300

    first

---
+++ get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+size_t			ft_strlen(const char *s);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdupc(char *s1, char c);
+int				get_next_line(int fd, char **line);
+
+#endif
